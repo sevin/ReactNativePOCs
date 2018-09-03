3a6c9c396d3644cad8feb76588434e5fe6c1a679
sevin <sevin.lai@ufinity.com>
2018-09-03 10:49:42 +0800

    Configured eslint and fixed eslint warnings

--- src/Components/DonutGraph.js
+++ src/Components/DonutGraph.js
@@ -1,27 +1,11 @@
 import React from 'react';
 
-import Svg,{
-    Circle,
-    Ellipse,
-    G,
-    LinearGradient,
-    RadialGradient,
-    Line,
-    Path,
-    Polygon,
-    Polyline,
-    Rect,
-    Symbol,
-    Text as SVGText,
-    Use,
-    Defs,
-    Stop
+import Svg, {
+  Path
 } from 'react-native-svg';
 
 export default class DonutGraph extends React.Component {
-  getRadian = (degrees) => {
-    return degrees * Math.PI / 180;
-  }
+  getRadian = degrees => degrees * Math.PI / 180;
 
   getEndXCoor = (cX, radius, degrees) => {
     const radian = this.getRadian(degrees);
@@ -36,66 +20,80 @@ export default class DonutGraph extends React.Component {
   getLargeArcSweepFlat = (degrees) => {
     if (degrees > 180) {
       return '1';
-    } else {
-      return '0';
     }
+    return '0';
   }
 
   getArcPath = (startX, startY, endX, endY, radius, degrees) => {
     const largeArcSweepFlat = this.getLargeArcSweepFlat(degrees);
-    const path = 'M '.concat(startX).concat(' ').concat(startY).concat(' A ').concat(radius).concat(' ').concat(radius).concat(' 0 ').concat(largeArcSweepFlat).concat(' 1 ').concat(endX).concat(' ').concat(endY);
+    const path = 'M '.concat(startX).concat(' ').concat(startY).concat(' A ')
+      .concat(radius)
+      .concat(' ')
+      .concat(radius)
+      .concat(' 0 ')
+      .concat(largeArcSweepFlat)
+      .concat(' 1 ')
+      .concat(endX)
+      .concat(' ')
+      .concat(endY);
     return path;
   }
 
   render() {
-    const radius = this.props.radius;
+    const {
+      radius, selectedArcStrokeWidth, degree, degreeTwo, degreeThree, selectedArc, arcStrokeWidth
+    } = this.props;
 
-    //calculate center of circle and size of SVG component using radius and selectedArcStrokeWidth
-    const cX = radius + this.props.selectedArcStrokeWidth/2;
-    const cY = radius + this.props.selectedArcStrokeWidth/2;
-    const svgHeight = 2 * radius + this.props.selectedArcStrokeWidth;
-    const svgWidth = 2 * radius + this.props.selectedArcStrokeWidth;
+    // calculate center of circle and size of SVG component using radius and selectedArcStrokeWidth
+    const cX = radius + selectedArcStrokeWidth / 2;
+    const cY = radius + selectedArcStrokeWidth / 2;
+    const svgHeight = 2 * radius + selectedArcStrokeWidth;
+    const svgWidth = 2 * radius + selectedArcStrokeWidth;
 
-    const fullCirclePath = 'M ' + cX + ' ' + cY + ' m 0 -' + radius + ' a ' + radius + ' ' + radius + ' 0 1 0 0 ' + 2*radius + ' a ' + radius + ' ' + radius + ' 0 1 0 0 -' + 2*radius;
+    const fullCirclePath = `M ${cX} ${cY} m 0 -${radius} a ${radius} ${radius} 0 1 0 0 ${2 * radius} a ${radius} ${radius} 0 1 0 0 -${2 * radius}`;
 
-    //===========formulating path of the first arc
-    const totalDegree = this.props.degree;
+    // formulating path of the first arc
+    const totalDegree = degree;
 
     const startX = cX;
     const startY = cY - radius;
     const endX = this.getEndXCoor(cX, radius, totalDegree);
     const endY = this.getEndYCoor(cY, radius, totalDegree);
-    const path = this.getArcPath(startX, startY, endX, endY, radius, this.props.degree);
+    const path = this.getArcPath(startX, startY, endX, endY, radius, degree);
 
 
-    //===========formulating path of the second arc
-    const totalDegree2 = this.props.degree + this.props.degreeTwo;
+    // formulating path of the second arc
+    const totalDegree2 = degree + degreeTwo;
 
     const endX2 = this.getEndXCoor(cX, radius, totalDegree2);
     const endY2 = this.getEndYCoor(cY, radius, totalDegree2);
-    const path2 = this.getArcPath(endX, endY, endX2, endY2, radius, this.props.degreeTwo);
+    const path2 = this.getArcPath(endX, endY, endX2, endY2, radius, degreeTwo);
 
 
-    //===========formulating path of the third arc
-    const totalDegree3 = this.props.degree + this.props.degreeTwo + this.props.degreeThree;
+    // formulating path of the third arc
+    const totalDegree3 = degree + degreeTwo + degreeThree;
 
     const endX3 = this.getEndXCoor(cX, radius, totalDegree3);
     const endY3 = this.getEndYCoor(cY, radius, totalDegree3);
-    const path3 = this.getArcPath(endX2, endY2, endX3, endY3, radius, this.props.degreeThree);
+    const path3 = this.getArcPath(endX2, endY2, endX3, endY3, radius, degreeThree);
 
 
-    //===========determing arcs' stroke width
-    let strokeWidth1, strokeWidth2, strokeWidth3;
-    strokeWidth1 = strokeWidth2 = strokeWidth3 = this.props.arcStrokeWidth;
-    switch (this.props.selectedArc) {
+    // determing arcs' stroke width
+    let strokeWidth1;
+    let strokeWidth2;
+    let strokeWidth3;
+    strokeWidth1 = arcStrokeWidth;
+    strokeWidth2 = arcStrokeWidth;
+    strokeWidth3 = arcStrokeWidth;
+    switch (selectedArc) {
       case 1:
-        strokeWidth1 = this.props.selectedArcStrokeWidth;
+        strokeWidth1 = selectedArcStrokeWidth;
         break;
       case 2:
-        strokeWidth2 = this.props.selectedArcStrokeWidth;
+        strokeWidth2 = selectedArcStrokeWidth;
         break;
       case 3:
-        strokeWidth3 = this.props.selectedArcStrokeWidth;
+        strokeWidth3 = selectedArcStrokeWidth;
         break;
       default:
         break;
@@ -108,25 +106,25 @@ export default class DonutGraph extends React.Component {
       >
         <Path
           d={fullCirclePath}
-          fill='transparent'
-          stroke='orange'
+          fill="transparent"
+          stroke="orange"
         />
         <Path
           d={path}
-          fill='transparent'
-          stroke='green'
+          fill="transparent"
+          stroke="green"
           strokeWidth={strokeWidth1}
         />
         <Path
           d={path2}
-          fill='transparent'
-          stroke='blue'
+          fill="transparent"
+          stroke="blue"
           strokeWidth={strokeWidth2}
         />
         <Path
           d={path3}
-          fill='transparent'
-          stroke='red'
+          fill="transparent"
+          stroke="red"
           strokeWidth={strokeWidth3}
         />
       </Svg>
--- src/Screens/GraphScreen.js
+++ src/Screens/GraphScreen.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
-
-import Svg,{
-    Circle,
-    Ellipse,
-    G,
-    LinearGradient,
-    RadialGradient,
-    Line,
-    Path,
-    Polygon,
-    Polyline,
-    Rect,
-    Symbol,
-    Text as SVGText,
-    Use,
-    Defs,
-    Stop
-} from 'react-native-svg';
+import {
+  StyleSheet, View, TouchableOpacity, Text,
+} from 'react-native';
 
 import DonutGraph from '../Components/DonutGraph';
+
+const styles = StyleSheet.create({
+  touchable: {
+    height: 40,
+    width: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+  },
+});
 
 export default class GraphScreen extends React.Component {
   constructor(props) {
@@ -28,14 +22,16 @@ export default class GraphScreen extends React.Component {
   }
 
   render() {
+    const { degree, degreeTwo, degreeThree } = this.state;
+
     return (
       <View>
         <View style={{ flexDirection: 'row', justifyContent: 'center', marginVertical: 20 }}>
           <DonutGraph
             radius={80}
-            degree={this.state.degree}
-            degreeTwo={this.state.degreeTwo}
-            degreeThree={this.state.degreeThree}
+            degree={degree}
+            degreeTwo={degreeTwo}
+            degreeThree={degreeThree}
             arcStrokeWidth={10}
             selectedArcStrokeWidth={10}
             selectedArc={0}
@@ -43,57 +39,53 @@ export default class GraphScreen extends React.Component {
         </View>
 
         <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-            <TouchableOpacity
-              style={[styles.touchable, { backgroundColor: 'pink' }]}
-              onPress={() => this.setState({ degree: this.state.degree-10 })}>
-              <Text>minus</Text>
-            </TouchableOpacity>
-            <Text>degree: {this.state.degree}</Text>
-            <TouchableOpacity
-              style={[styles.touchable, { backgroundColor: 'pink' }]}
-              onPress={() => this.setState({ degree: this.state.degree+10 })}>
-              <Text>plus</Text>
-            </TouchableOpacity>
-          </View>
+          <TouchableOpacity
+            style={[styles.touchable, { backgroundColor: 'pink' }]}
+            onPress={() => this.setState({ degree: degree - 10 })}
+          >
+            <Text>minus</Text>
+          </TouchableOpacity>
+          <Text>degree:{' '}{degree}</Text>
+          <TouchableOpacity
+            style={[styles.touchable, { backgroundColor: 'pink' }]}
+            onPress={() => this.setState({ degree: degree + 10 })}
+          >
+            <Text>plus</Text>
+          </TouchableOpacity>
+        </View>
 
-          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-            <TouchableOpacity
-              style={[styles.touchable, { backgroundColor: 'pink' }]}
-              onPress={() => this.setState({ degreeTwo: this.state.degreeTwo-10 })}>
-              <Text>minus</Text>
-            </TouchableOpacity>
-            <Text>degree: {this.state.degreeTwo}</Text>
-            <TouchableOpacity
-              style={[styles.touchable, { backgroundColor: 'pink' }]}
-              onPress={() => this.setState({ degreeTwo: this.state.degreeTwo+10 })}>
-              <Text>plus</Text>
-            </TouchableOpacity>
-          </View>
+        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+          <TouchableOpacity
+            style={[styles.touchable, { backgroundColor: 'pink' }]}
+            onPress={() => this.setState({ degreeTwo: degreeTwo - 10 })}
+          >
+            <Text>minus</Text>
+          </TouchableOpacity>
+          <Text>degree:{' '}{degreeTwo}</Text>
+          <TouchableOpacity
+            style={[styles.touchable, { backgroundColor: 'pink' }]}
+            onPress={() => this.setState({ degreeTwo: degreeTwo + 10 })}
+          >
+            <Text>plus</Text>
+          </TouchableOpacity>
+        </View>
 
-          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-            <TouchableOpacity
-              style={[styles.touchable, { backgroundColor: 'pink' }]}
-              onPress={() => this.setState({ degreeThree: this.state.degreeThree-10 })}>
-              <Text>minus</Text>
-            </TouchableOpacity>
-            <Text>degree: {this.state.degreeThree}</Text>
-            <TouchableOpacity
-              style={[styles.touchable, { backgroundColor: 'pink' }]}
-              onPress={() => this.setState({ degreeThree: this.state.degreeThree+10 })}>
-              <Text>plus</Text>
-            </TouchableOpacity>
-          </View>
+        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+          <TouchableOpacity
+            style={[styles.touchable, { backgroundColor: 'pink' }]}
+            onPress={() => this.setState({ degreeThree: degreeThree - 10 })}
+          >
+            <Text>minus</Text>
+          </TouchableOpacity>
+          <Text>degree:{' '}{degreeThree}</Text>
+          <TouchableOpacity
+            style={[styles.touchable, { backgroundColor: 'pink' }]}
+            onPress={() => this.setState({ degreeThree: degreeThree + 10 })}
+          >
+            <Text>plus</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  touchable: {
-    height: 40,
-    width: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 10
-  }
-});
